f41fd06fbdb8711c3125620e4cb09f6faf51ca68
Brandon <bbeam1@gmail.com>
2022-11-15 19:45:40 -0700

    assert arrays equal practice

---
+++ assertArraysEqual.js
@@ -0,0 +1,12 @@
+let assertArraysEqual = function(one,two) {
+  for (let num of one) {
+    if (one[num] !== two[num]) {
+      return console.log('sadness')
+      } 
+  } return console.log('yay')
+} 
+assertArraysEqual([1,2,3],[1,2,5])
+assertArraysEqual([1,2,3],[1,2,3])
+assertArraysEqual([1,2,3],[1,2,5])
+assertArraysEqual(['1',2,3],['1',2,3])
+assertArraysEqual([1,2,3],[1,2,3])
