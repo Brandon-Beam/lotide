ce80d4190bd5cb2753448567cd74924422525976
Brandon <bbeam1@gmail.com>
2022-11-16 22:03:05 -0700

    find my keys

---
+++ findKeyByValue.js
@@ -0,0 +1,22 @@
+const findKeyByValue = (object, value) => {
+  for (const item in object) {
+    if (object[item] === value) {
+      return item
+    }
+  }
+}
+let assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} ===  + ${expected}`);
+  } else {
+    console.log(`😭Assertion Failed: ${actual} !==  + ${expected}`);
+  }
+};
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
