339a1e2003830d2e45c94a831b70da898e14bf64
Brandon <bbeam1@gmail.com>
2022-11-17 20:25:34 -0700

    map logic written out

---
+++ map.js
@@ -0,0 +1,21 @@
+const numbers = [1,2,3,4,5,6]
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = (array, callback) => {
+  
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item))
+  } return results;
+}
+let assertArraysEqual = function(one,two) {
+  for (let num of one) {
+    if (one[num] !== two[num]) {
+       console.log('sadness')
+       return
+      } 
+  }  console.log('yay')
+} 
+assertArraysEqual(map(words, word => word[0]),words.map(word => word[0]));
+assertArraysEqual(map(numbers,irreleventName => irreleventName * 5),numbers.map(AnotherIrreleventName => AnotherIrreleventName * 5))
+assertArraysEqual(map(numbers,x=>x++),numbers.map(x=>x++))
