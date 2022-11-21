4fa861a351c0c3cd01e50c23725b395cdca3ac88
Brandon <bbeam1@gmail.com>
2022-11-21 13:58:58 -0700

    eqArray refactor

--- eqArray.js
+++ eqArray.js
@@ -1,20 +1,9 @@
-let assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} ===  + ${expected}`);
-  } else {
-    console.log(`😭Assertion Failed: ${actual} !==  + ${expected}`);
-  }
-};
-let eqArrays = function(one,two) {
+let eqArray = function (one, two) {
   for (let num of one) {
     if (one[num] !== two[num]) {
       return false
-    } 
+    }
   } return true
-} 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+};
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArray
