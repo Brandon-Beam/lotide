0f904f51d92bce8887382b8d48a7a2789e38f559
Brandon <bbeam1@gmail.com>
2022-11-21 13:37:38 -0700

    first 2 assignments dried

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
-let assertEqual = function(actual, expected) {
+let assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`😭Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual
--- head.js
+++ head.js
@@ -1,12 +1,6 @@
-let assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} ===  + ${expected}`);
-  } else {
-    console.log(`😭Assertion Failed: ${actual} !==  + ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 let head = function (i) {
   return i[0]
 };
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head
