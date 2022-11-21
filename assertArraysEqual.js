159f7db449203872c4c3c67badd034ddb04425eb
Brandon <bbeam1@gmail.com>
2022-11-21 14:19:09 -0700

    assertArraysEqual dried off

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,10 @@
-let assertArraysEqual = function(one,two) {
+let assertArraysEqual = function (one, two) {
   for (let num of one) {
     if (one[num] !== two[num]) {
-       console.log('sadness')
-       return
-      } 
-  }  console.log('yay')
-} 
-assertArraysEqual([1,2,3],[1,2,5])
-assertArraysEqual([1,2,3],[1,2,3])
-assertArraysEqual([1,2,3],[1,2,5])
-assertArraysEqual(['1',2,3],['1',2,3])
-assertArraysEqual([1,2,3],[1,2,3])
+      console.log('sadness')
+      return
+    }
+  } console.log('yay')
+}
+
+module.exports = assertArraysEqual
