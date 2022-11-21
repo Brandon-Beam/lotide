f4bade7449fa561da957269b502f48437f421c9a
Brandon <bbeam1@gmail.com>
2022-11-21 14:31:50 -0700

    middle refactored

--- middle.js
+++ middle.js
@@ -1,17 +1,3 @@
-let assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} ===  + ${expected}`);
-  } else {
-    console.log(`😭Assertion Failed: ${actual} !==  + ${expected}`);
-  }
-};
-let eqArrays = function (one, two) {
-  for (let num of one) {
-    if (one[num] !== two[num]) {
-      return false
-    }
-  } return true
-}
 const middle = (list) => {
   for (i = 0; i < list.length; i++) {
     list.shift()
@@ -19,13 +5,7 @@ const middle = (list) => {
     if (list.length <= 2) {
       break;
     }
-  } console.log(list)
+  } return list
 }
 
-
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-middle([1]) // => []
-middle([1, 2]) // => []
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
+module.exports = middle
